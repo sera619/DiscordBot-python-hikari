4fe311bea1edc6deabae03db9d7008c3a0a05fd4
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-09-14 20:06:52 +0200

    add byte dictionary saving for calender

--- plugins/calender.py
+++ plugins/calender.py
@@ -1,6 +1,8 @@
 import hikari
 import miru
 import lightbulb
+import os
+import pickle
 
 
 
@@ -18,7 +20,16 @@ def SaveRaidTerms(date, time, raid):
     base_raid_term['date'] = str(date)
     base_raid_term['raid'] = str(raid)
     base_raid_term['time'] = str(time)
-    print(base_raid_term)
+    with open(RAID_TERMS_PATH, 'wb') as f:
+        pickle.dump(base_raid_term, f)
+
+
+def LoadRaidTerms():
+    with open(RAID_TERMS_PATH,'rb') as f:
+        loaded_dict = pickle.load(f)
+        print(loaded_dict)
+        return loaded_dict
+
 
 @calendar_plugin.command
 @lightbulb.command('calendar', 'All Calendar relevant commands')
@@ -31,10 +42,16 @@ async def calendarCommands(ctx):
 @lightbulb.command('show', 'shows the current calendar entrys', auto_defer= True, pass_options = True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def showCalendar(ctx: lightbulb.Context):
+    raid_term = LoadRaidTerms()
+    time = raid_term['time']
+    date = raid_term['date']
+    raid = raid_term['raid']
     new_embded = hikari.Embed(
-        title='New Calendar',
-        description='Your current calendar entrys.'
+        title='Raid Calendar',
+        description='Your current calendar entrys.\n\n'
+        f"\nTime: **{str(time)}**\nDate: **{str(date)}**\nRaid: **{str(raid)}**",
     )
+    
 
     await ctx.respond(embed=new_embded)
 
