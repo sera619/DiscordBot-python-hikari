ab31bd4b2120631811b5fd06aac5f51ef47cc916
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-04 15:34:24 +0100

    add startup change command

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,9 +1,21 @@
 import hikari
 import lightbulb
+import asyncio
 
 from .commands import checkAdmin
-from .config import ROLE_HPS_USERS, ROLE_TANK_USERS, SERA_ID, ROLE_DPS_USERS
+from .config import ROLE_HPS_USERS, ROLE_TANK_USERS, SERA_ID, ROLE_DPS_USERS, SHOW_START_EMBED, load_bot_setting
 admin_plugin = lightbulb.Plugin('admin')
+
+
+async def SetGuildRules():
+    await asyncio.sleep(2.0)
+    print("2Sec waited")
+    pass
+
+
+async def save_bot_setting(t):
+    with open('./data/bot-set.txt', 'w') as f:
+        f.write(str(t))
 
 ### Templates to send ingame role lists to discord channel
 async def getTankRoles(ctx: lightbulb.Context):
@@ -80,14 +92,30 @@ async def adminInitBot(ctx: lightbulb.Context):
     channels = await admin_plugin.bot.rest.fetch_guild_channels(ctx.guild_id)
     control_exisits = False
     for channel in channels:
-        if channel.name == 'necrobot':
+        if channel.name == 'bot-commands':
             control_exisits = True
             return await ctx.respond('NecroBot control text channel already exists.')
 
     if not control_exisits:
         control_exisits = True
         await ctx.edit_last_response('No control channel found, created one.')
-        await admin_plugin.bot.rest.create_guild_text_channel(ctx.guild_id, "necrobot")
+        return await admin_plugin.bot.rest.create_guild_text_channel(ctx.guild_id, "bot-commands")
+
+
+@adminCommands.child
+@lightbulb.command('changestart', 'Activate / Deactivate the bot startmessage.')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def changestart(ctx: lightbulb.Context):
+    if not await checkAdmin(ctx.author.id, ctx.guild_id):
+        return
+
+    load =  load_bot_setting()
+    if load == False:
+        await save_bot_setting(1)
+        await ctx.respond('**Start Output will be shown next start.**')
+    else:
+        await save_bot_setting(2)
+        await ctx.respond("**Start Output will be not shown next start.**")
 
 
 @adminCommands.child
@@ -111,15 +139,22 @@ async def adminGetDpsRoles(ctx: lightbulb.Context):
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def adminGetTankRoles(ctx: lightbulb.Context):
     await getTankRoles(ctx)
-    await ctx.respond('**DPS list request done!**')
+    await ctx.respond('**Tank list request done!**')
 
 @adminCommands.child
 @lightbulb.command('hpsroles', "Get all user with ingame HPS role")
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def adminGetHpsRoles(ctx: lightbulb.Context):
     await getHPSRoles(ctx)
-    await ctx.respond('**DPS list request done!**')
+    await ctx.respond('**HPS list request done!**')
 
+
+@adminCommands.child
+@lightbulb.command('rules', "Set the Rules for your guild and post to your rulechannel")
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def setRules(ctx: lightbulb.Context):
+    await SetGuildRules()
+    await ctx.respond("Rules test command finished")
 ################################################
 
 def load(bot):
