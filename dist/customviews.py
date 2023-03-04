6b34c50ec7b3dba1877ceaddb51078c256354641
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-04 15:34:12 +0100

    create RemoveCall / Remove roll views

--- dist/customviews.py
+++ dist/customviews.py
@@ -3,24 +3,18 @@ import hikari
 from hikari import emojis
 import random
 
-
-from plugins.config import (AddDpsRole,
-    AddHpsRole,
-    AddTankRole,
-    SHOW_START_EMBED,
-    WoWClassHandler)
+from plugins.config import AddDpsRole,AddHpsRole,AddTankRole,SHOW_START_EMBED,WoWClassHandler, RemoveDpsRole,RemoveHpsRole,RemoveTankRole
 
 class DiceView(miru.View):
-    @miru.select(
-        placeholder="Choose a color ...",
-        options=[
-            miru.SelectOption(label="Red"),
-            miru.SelectOption(label="Green"),
-            miru.SelectOption(label="Blue")
-        ]
-    )
-    async def slm_colours(self, select: miru.Select, ctx: miru.Context):
-        await ctx.respond(f"{select.values[0]} is the color you choosed!")
+    # @miru.select.base( placeholder="Choose a color ...",
+    #     options=[
+    #         miru.SelectOption(label="Red"),
+    #         miru.SelectOption(label="Green"),
+    #         miru.SelectOption(label="Blue")
+    #     ]
+    # )
+    # async def slm_colours(self, select: miru.SelectOption, ctx: miru.Context):
+    #     await ctx.respond(f"{select.values[0]} is the color you choosed!")
 
     # Create button
     @miru.button(emoji='6️⃣', style=hikari.ButtonStyle.PRIMARY)
@@ -45,19 +39,130 @@ class DiceView(miru.View):
 class StartOptionView(miru.View):
     @miru.button(emoji="✅", style=hikari.ButtonStyle.SECONDARY)
     async def showStartEmbed(self, button: miru.Button, ctx: miru.Context):
-        SHOW_START_EMBED = True
+
         await ctx.edit_response("Zeige Start Nachricht "+str(SHOW_START_EMBED), components=[])
 
-    @miru.button(emoji="❌", style=hikari.ButtonStyle.DANGER)
+    @miru.button(emoji="❌", style=hikari.ButtonStyle.PRIMARY)
     async def hideStartEmbed(self, button:miru.Button, ctx: miru.Context):
-        SHOW_START_EMBED = False
         await ctx.edit_response("Zeige Start Nachricht: "+str(SHOW_START_EMBED), components=[])
 
-class ClassView(miru.View):
     
+    async def on_timeout(self):
+        await self.message.edit("The changestart timed out.", components=[])
+        self.stop()
+
+
+class RemoveClassView(miru.View):
     @miru.button(emoji=1017789881029763123,label="Death Knight", style=hikari.ButtonStyle.PRIMARY)
     async def setClassDK(self, button: miru.Button, ctx: miru.Context):
-        if WoWClassHandler().AddClassDK(new_dps=ctx.user) == True:
+        if WoWClassHandler().RemoveDkPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Death Knight Class.", components=[])
+        else:
+            await ctx.edit_response('You arent a Death Knight!', components=[])
+        self.stop()
+
+    @miru.button(emoji= 1017789934440026233,label="Demon Hunter", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassDH(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveDHPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the DH Class", components=[])
+        else:
+            await ctx.edit_response("You arent a Demon Hunter!", components=[])
+        self.stop()
+    
+    @miru.button(emoji=1017789969059819541,label="Druid", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassDruid(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveDruidPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Druid Class", components=[])
+        else:
+            await ctx.edit_response("You arent a Druid!", components=[])
+        self.stop()
+
+    @miru.button(emoji=1017790009685852201, label="Hunter", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassHunter(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveHunterPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Hunter Class", components=[])
+        else:
+            await ctx.edit_response("You arent a Hunter!", components=[])
+        self.stop()
+    
+    @miru.button(emoji=1017790054824947753,label="Mage", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassDH(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveMagePlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Mage Class", components=[])
+        else:
+            await ctx.edit_response("You arent a Mage!", components=[])
+        self.stop()
+    
+    @miru.button(emoji=1017790056334889040,label="Monk", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassMonk(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveMonkPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Monk Class", components=[])
+        else:
+            await ctx.edit_response("You arent a Monk", components=[])
+        self.stop()
+    
+    @miru.button(emoji=1017790057530265621,label="Paladin", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassPaladin(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemovePaladinPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved Paladin Class", components=[])
+        else:
+            await ctx.edit_response("You arent a Paladin!", components=[])
+        self.stop()
+
+    @miru.button(emoji=1017790058851483720,label="Priest", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassPriest(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemovePriestPlayer(ctx.user) == True:
+            await ctx.edit_response("You Choosed Priest Class", components=[])
+        else:
+            await ctx.edit_response("You already a Priest!", components=[])
+        self.stop()
+
+
+    @miru.button(emoji=1017790059673563217, label='Rogue', style=hikari.ButtonStyle.PRIMARY)
+    async def setClassRogue(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveRougePlayer(ctx.user) == True:
+            await ctx.edit_response('You leaved the Rogue Class.', components=[])
+        else:
+            await ctx.edit_response("You arent a Rogue!", components=[])
+        self.stop()
+
+
+    @miru.button(emoji=1017790061401620500 ,label="Shaman", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassShaman(self, button: miru.Button, ctx: miru.Context,):
+        if WoWClassHandler().RemoveShamanPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Shaman Class", components=[])
+        else:
+            await ctx.edit_response('You arent a Shaman!', components=[])
+        self.stop()
+
+
+    @miru.button(emoji=1017790063310024774,label="Warlock", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassWarlock(self, button: miru.Button ,ctx: miru.Context):
+        if WoWClassHandler().RemoveWarlockPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Warlock Class", components=[])
+        else:
+            await ctx.edit_response('You arent a Warlock!', components=[])
+        self.stop()
+
+
+    @miru.button(emoji=1017790064698343496,label="Warrior", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassWarrior(self,button:miru.Button, ctx: miru.Context):
+        if WoWClassHandler().RemoveWarriorPlayer(ctx.user) == True:
+            await ctx.edit_response("You leaved the Warrior Class", components=[])
+        else:
+            await ctx.edit_response('You arent a Warrior!', components=[])
+        self.stop()
+    
+    async def on_timeout(self):
+        await self.message.edit("The classcheck timed out.", components=[])
+        self.stop()
+
+
+
+class ClassView(miru.View):
+    @miru.button(emoji=1017789881029763123,label="Death Knight", style=hikari.ButtonStyle.PRIMARY)
+    async def setClassDK(self, button: miru.Button, ctx: miru.Context):
+        if WoWClassHandler().AddClassDK(ctx.user) == True:
             await ctx.edit_response("You choosed Death Knight Class.", components=[])
         else:
             await ctx.edit_response('You already a Death Knight!', components=[])
@@ -65,7 +170,7 @@ class ClassView(miru.View):
 
     @miru.button(emoji= 1017789934440026233,label="Demon Hunter", style=hikari.ButtonStyle.PRIMARY)
     async def setClassDH(self, button: miru.Button, ctx: miru.Context):
-        if WoWClassHandler().AddClassDH(new_dps=ctx.user) == True:
+        if WoWClassHandler().AddClassDH(ctx.user) == True:
             await ctx.edit_response("You Choosed DH Class", components=[])
         else:
             await ctx.edit_response("You already a Demon Hunter!", components=[])
@@ -191,3 +296,32 @@ class RoleView(miru.View):
     async def on_timeout(self):
         await self.message.edit("The menu timed out.", components=[])
         self.stop()
+
+
+class RollRemoveView(miru.View):
+    @miru.button(emoji="⚔️", label="DPS", style=hikari.ButtonStyle.DANGER)
+    async def remove_dps_role(self, button: miru.Button, ctx: miru.Context):
+        new_embed = hikari.Embed(
+            title="Character Role Remove",
+            description=" **You have leave the dps role!**",
+            )
+        RemoveDpsRole(str(ctx.user))
+        return await ctx.edit_response(embed= new_embed, components=[])
+
+    @miru.button(emoji="💟", label='HPS', style=hikari.ButtonStyle.SUCCESS)
+    async def remove_hps_role(self, button: miru.Button, ctx: miru.Context):
+        new_embed = hikari.Embed(
+            title="Character Role Remove",
+            description=" **You have leave the hps role!**",
+            )
+        RemoveHpsRole(str(ctx.user))
+        return await ctx.edit_response(embed= new_embed, components=[])
+    
+    @miru.button(emoji="🛡️", label='Tank', style=hikari.ButtonStyle.SECONDARY)
+    async def remove_tank_role(self, button: miru.Button, ctx: miru.Context):
+        new_embed = hikari.Embed(
+            title="Character Role Remove",
+            description=" **You have leave the tank role!**",
+            )
+        RemoveTankRole(str(ctx.user))        
+        return await ctx.edit_response(embed= new_embed, components=[])
